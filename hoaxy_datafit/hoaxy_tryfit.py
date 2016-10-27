1844288b11b01f28a3ce07ca8c89c1369cc663b7
tambu85 <marcella.tambuscio@gmail.com>
2016-10-27 18:56:41 +0200

    Add files via upload

---
+++ hoaxy_datafit/hoaxy_tryfit.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import pylab
+import scipy
+import pandas as pd
+from scipy.optimize import curve_fit
+from  models_functions import mf_seg, mf_noseg, sir, mf_seg_plot, mf_noseg_plot
+
+
+hoax_data = pd.read_csv('hoaxy_mexico-border-trump-elected_cum.csv',parse_dates=True)
+for_users = hoax_data['For']
+against_users = hoax_data['Against']
+t_empirical=np.arange(0,len(hoax_data))
+len_data= len(hoax_data)
+
+#DATA FITTING
+#mf_seg
+param0=[0.1,0.5, 7000, 0.5, 0.1, 0.1, 0.8, 10]
+param_bounds =([0.001,0, 1000, 0.1, 0, 0, 0.5, 5], [10.,1., 2000000, 0.9, 0.9, 0.2, 1., 15])
+
+popt, pcov = scipy.optimize.curve_fit(mf_seg,
+                                          t_empirical,
+                                          for_users,
+                                          p0=param0,
+                                          bounds=param_bounds)
+
+[tfact,alpha, net_size, gullible_size, bainit, pvsk, segregation, tau] = popt
+print('MF WITH SEGREGATION')
+print(pcov)
+print('tfact='+str(tfact))
+print('alpha = '+ str(round(alpha, 2)))
+print('network size = ' + str(round(net_size)))
+print('gullible size= ' + str(round(gullible_size, 2)))
+print(' initial BA = ' + str(round(bainit, 2)))
+print('verify SK = ' + str(round(pvsk, 2)))
+print('segregation = ' + str(round(segregation, 2)))
+print('tau = ' + str(round(tau)))
+print('*****************')
+
+
+# plot fit and data
+t_plot = np.arange(0, len_data+tfact, tfact)
+
+for j in range(0, len(t_plot)):
+    t_plot[j] = round(t_plot[j], 5)
+
+t_plot= np.unique(np.sort(np.append(t_plot, t_empirical)))
+ba_plot=mf_seg_plot(t_plot, alpha, net_size, gullible_size, bainit, pvsk, segregation, tau)
+
+pylab.figure()
+pylab.axis([0.0,max(t_empirical), 0, net_size / 2])
+ax = pylab.gca()
+ax.set_autoscale_on(False)
+pylab.plot(t_empirical, for_users, '.', label='data point')
+pylab.plot(t_plot,ba_plot , label='fit_seg')
+
+#print(abs(ba - for_users))
+
+#mf_noseg
+param0=[0.1,0.3, 7000, 0.05, 0, 10]
+param_bounds =([0.001,0, 1000, 0, 0,  5], [10,1., 2000000, 0.3, 0.05, 15])
+#t_sim = scipy.linspace(0, len(hoax_data), 180)
+popt, pcov = scipy.optimize.curve_fit(mf_noseg,
+                                        t_empirical,
+                                        for_users,
+                                        p0=param0,
+                                        bounds=param_bounds)
+
+[tfact, alpha, net_size,  bainit, pv, tau] = popt
+print('MF WITHOUT SEGREGATION')
+print(pcov)
+print('tfact=' + str(tfact))
+print('alpha = ' + str(round(alpha, 2)))
+print('network size = ' + str(round(net_size)))
+print(' initial BA = ' + str(round(bainit, 2)))
+print('verify = ' + str(round(pv, 2)))
+print('tau = ' + str(round(tau)))
+#
+
+# plot fit and data
+
+t_plot = np.arange(0, len_data+tfact, tfact)
+for j in range(0, len(t_plot)):
+    t_plot[j] = round(t_plot[j], 5)
+
+t_plot= np.unique(np.sort(np.append(t_plot, t_empirical)))
+ba_plot= mf_noseg_plot(t_plot, alpha, net_size, bainit, pv, tau)
+pylab.plot(t_plot, ba_plot, label='fit_noseg')
+
+#sir
+param0=[0.75, 0.5, 0., 1600]
+param_bounds =([0, 0, 0, 1000], [1., 1., 1.,  2000000])
+
+popt, pcov = scipy.optimize.curve_fit(sir, t_empirical,
+                                           for_users,
+                                           p0=param0,
+                                           bounds=param_bounds)
+
+[bainit, beta,  mu, net_size] = popt
+print('SIR')
+print(pcov)
+print('beta = ' + str(round(beta, 2)))
+print('network size = ' + str(round(net_size)))
+print(' initial BA = ' + str(round(bainit, 2)))
+print('mu = ' + str(round(mu, 2)))
+#
+infected=sir(t_empirical,  bainit, beta, mu, net_size)
+pylab.plot(t_empirical, infected, label='sir')
+
+pylab.title('FIT PARAMETERS')
+pylab.legend()
+pylab.legend(['For Users', 'Model with segregation', 'Model without segregation', "SIR"])
+pylab.savefig('hoaxy_mexico_tryfit.pdf', format='pdf')
+
+
+
+
+
---
+++ hoaxy_datafit/models_functions.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.optimize import curve_fit
+import scipy, scipy.integrate
+
+#our model without segregation (random network)
+def mf_noseg(T,tfact, a,netsize, ba_init,verify, dec):
+
+    ba=[]
+    fa=[]
+    alpha=a
+    pv=verify
+
+    N=netsize
+    tau=dec
+    pBI0=0.0
+    pBA0=ba_init
+    pFI0=0.0
+    pFA0=0.0
+    pS0=1.-(pFA0+pFI0+pBA0+pBI0)
+
+    T_sim = np.arange(min(T), max(T) + tfact, tfact)
+
+    for j in range(0, len(T_sim)):
+        T_sim[j] = round(T_sim[j], 5)
+
+    T_sim = np.sort(np.append(T_sim, T))
+    T_sim_sorted = np.unique(T_sim)
+
+    for i in T_sim_sorted:
+
+        if (i in T):
+            ab = int(float(pBA0) * N)
+            ba.append(ab)
+
+        f = pBA0
+        pBI1 = alpha * f * pS0 + (1. / tau) * pBA0 + (1. - pv) * (1. - f) * pBI0
+        pBA1 = (1. - pv) * f  * pBI0 + (1. - (1. / tau)) * (1. - pv) * pBA0
+        pFI1 = (1-alpha) * f * pS0 + (1. / tau) * pFA0 + pv * (pBI0 + (1. - 1. / tau) * pBA0) + (1 - f) * pFI0
+        pFA1 = f * pFI0 + (1. - 1. / tau) * pFA0
+        pS1 = (1. - f) * pS0
+
+        #update
+        pBI0 = pBI1
+        pBA0 = pBA1
+        pFI0 = pFI1
+        pFA0 = pFA1
+        pS0 = pS1
+
+        if float(pBI0)<=0.0000000001:
+            pBI0=0.0
+        if float(pBA0)<=0.0000000001:
+            pBA0=0.0
+        if float(pFI0)<=0.0000000001:
+            pFI0=0.0
+        if float(pFA0)<=0.0000000001:
+            pFA0=0.0
+        if float(pS0)<=0.0000000001:
+            pS0=0.0
+
+    return (np.asarray(ba))
+
+def mf_noseg_plot(T, a,netsize, ba_init,verify, dec):
+
+    ba=[]
+    alpha=a
+    pv=verify
+    N=netsize
+    tau=dec
+
+    pBI0=0.0
+    pBA0=ba_init
+    pFI0=0.0
+    pFA0=0.0
+    pS0=1.-(pFA0+pFI0+pBA0+pBI0)
+
+
+    for i in T:
+
+        ab = int(float(pBA0) * N)
+        ba.append(ab)
+
+        f = pBA0
+        pBI1 = alpha * f * pS0 + (1. / tau) * pBA0 + (1. - pv) * (1. - f) * pBI0
+        pBA1 = (1. - pv) * f  * pBI0 + (1. - (1. / tau)) * (1. - pv) * pBA0
+        pFI1 = (1-alpha) * f * pS0 + (1. / tau) * pFA0 + pv * (pBI0 + (1. - 1. / tau) * pBA0) + (1 - f) * pFI0
+        pFA1 = f * pFI0 + (1. - 1. / tau) * pFA0
+        pS1 = (1. - f) * pS0
+
+        #update
+        pBI0 = pBI1
+        pBA0 = pBA1
+        pFI0 = pFI1
+        pFA0 = pFA1
+        pS0 = pS1
+
+        if float(pBI0)<=0.0000000001:
+            pBI0=0.0
+        if float(pBA0)<=0.0000000001:
+            pBA0=0.0
+        if float(pFI0)<=0.0000000001:
+            pFI0=0.0
+        if float(pFA0)<=0.0000000001:
+            pFA0=0.0
+        if float(pS0)<=0.0000000001:
+            pS0=0.0
+
+    return (np.asarray(ba))
+
+
+#our model with segregation (two clusters gullible-skeptic)
+def mf_seg(T,tfact,a,netsize, gulsize, ba_init,verify_sk,segregation, dec):
+
+    b=[]
+    fa=[]
+    alpha=a
+    s=segregation
+    pvg=0.
+    pvsk=verify_sk
+    gullible_size=gulsize*netsize
+    N=netsize
+    skeptic_size=N-gullible_size
+    tau=dec
+    pBI0g=0.0
+    pBA0g=ba_init
+    pFI0g=0.0
+    pFA0g=0.0
+    pS0g=1.-(pFA0g+pFI0g+pBA0g+pBI0g)
+    pBI0sk=0.0
+    pBA0sk=ba_init
+    pFI0sk=0.0
+    pFA0sk=0.0
+    pS0sk=1.-(pFA0sk+pFI0sk+pBA0sk+pBI0sk)
+
+    T_sim=np.arange(min(T),max(T)+tfact,tfact)
+
+    for j in range(0,len (T_sim)):
+        T_sim[j]=round(T_sim[j],5)
+
+    T_sim=np.sort(np.append(T_sim,T))
+    T_sim_sorted=np.unique(T_sim)
+
+
+    for i in T_sim_sorted:
+
+        if(i in T):
+            baG = int(float(pBA0g) * gullible_size)
+            baSK = int(float(pBA0sk) * skeptic_size)
+            #biG = int(float(pBI0g) * gullible_size)
+            #biSK = int(float(pBI0sk) * skeptic_size)
+
+            #b_tot = baG + baSK + biG + biSK
+            b_tot= baG + baSK
+            b.append(b_tot)
+
+        fg=s*pBA0g+(1-s)*pBA0sk
+        if(i>0):
+            pFI1g=(1.-alpha)*fg*pS0g+(1./tau)*pFA0g+pvg*(pBI0g+(1.-1./tau)*pBA0g)+(1.-fg)*pFI0g
+            pBI1g = alpha * fg * pS0g + (1. / tau) * pBA0g + (1. - pvg) * (1. - fg) * pBI0g
+            pBA1g = (1. - pvg) * fg * pBI0g + (1. - (1. / tau)) * (1. - pvg) * pBA0g
+        else:
+            pFI1g = (1. - alpha) * fg * pS0g + (1. / tau) * pFA0g  + (1. - fg) * pFI0g
+            pBI1g = alpha * fg * pS0g + (1. / tau) * pBA0g +  (1. - fg) * pBI0g
+            pBA1g =  fg * pBI0g + (1. - (1. / tau)) * pBA0g
+        pFA1g=fg*pFI0g+(1.-1./tau)*pFA0g
+        pS1g=(1.-fg)*pS0g
+
+        fsk=s*pBA0sk+(1-s)*pBA0g
+        pBI1sk=alpha*fsk*pS0sk+(1./tau)*pBA0sk+(1.-pvsk)*(1.-fsk)*pBI0sk
+        pBA1sk=(1.-pvsk)*fsk*pBI0sk+(1.-(1./tau))*(1.-pvsk)*pBA0sk
+        pFI1sk=(1.-alpha)*fsk*pS0sk+(1./tau)*pFA0sk+pvsk*(pBI0sk+(1.-1./tau)*pBA0sk)+(1.-fsk)*pFI0sk
+        pFA1sk=fsk*pFI0sk+(1.-1./tau)*pFA0sk
+        pS1sk=(1.-fsk)*pS0sk
+
+        #update
+        pBI0g = pBI1g
+        pBA0g = pBA1g
+        pFI0g = pFI1g
+        pFA0g = pFA1g
+        pS0g = pS1g
+
+        pBI0sk=pBI1sk
+        pBA0sk=pBA1sk
+        pFI0sk=pFI1sk
+        pFA0sk=pFA1sk
+        pS0sk=pS1sk
+
+        if float(pBI0sk)<=0.0000000001:
+            pBI0sk=0.0
+        if float(pBA0sk)<=0.0000000001:
+            pBA0sk=0.0
+        if float(pFI0sk)<=0.0000000001:
+            pFI0sk=0.0
+        if float(pFA0sk)<=0.0000000001:
+            pFA0sk=0.0
+        if float(pS0sk)<=0.0000000001:
+            pS0sk=0.0
+        if float(pBI0g)<=0.0000000001:
+	        pBI0g=0.0
+		if float(pBA0g)<=0.0000000001:
+			pBA0g=0.0
+		if float(pFI0g)<=0.0000000001:
+			pFI0g=0.0
+		if float(pFA0g)<=0.0000000001:
+			pFA0g=0.0
+		if float(pS0g)<=0.0000000001:
+			pS0g=0.0
+
+    return (np.asarray(b))
+
+
+def mf_seg_plot(T,a,netsize, gulsize, ba_init,verify_sk,segregation, dec):
+
+    b=[]
+    alpha=a
+    s=segregation
+    pvg=0.
+    pvsk=verify_sk
+    gullible_size=gulsize*netsize
+    N=netsize
+    skeptic_size=N-gullible_size
+    tau=dec
+
+    pBI0g=0.0
+    pBA0g=ba_init
+    pFI0g=0.0
+    pFA0g=0.0
+    pS0g=1.-(pFA0g+pFI0g+pBA0g+pBI0g)
+    pBI0sk=0.0
+    pBA0sk=ba_init
+    pFI0sk=0.0
+    pFA0sk=0.0
+    pS0sk=1.-(pFA0sk+pFI0sk+pBA0sk+pBI0sk)
+
+    for i in T:
+
+        baG = int(float(pBA0g) * gullible_size)
+        baSK = int(float(pBA0sk) * skeptic_size)
+        #biG = int(float(pBI0g) * gullible_size)
+        #biSK = int(float(pBI0sk) * skeptic_size)
+        b_tot = baG + baSK # + biG + biSK
+        b.append(b_tot)
+
+
+        fg=s*pBA0g+(1-s)*pBA0sk
+        pBI1g=alpha*fg*pS0g+(1./tau)*pBA0g+(1.-pvg)*(1.-fg)*pBI0g
+        pBA1g=(1.-pvg)*fg*pBI0g+(1.-(1./tau))*(1.-pvg)*pBA0g
+        pFI1g=(1.-alpha)*fg*pS0g+(1./tau)*pFA0g+pvg*(pBI0g+(1.-1./tau)*pBA0g)+(1.-fg)*pFI0g
+        pFA1g=fg*pFI0g+(1.-1./tau)*pFA0g
+        pS1g=(1.-fg)*pS0g
+
+        fsk=s*pBA0sk+(1-s)*pBA0g
+        pBI1sk=alpha*fsk*pS0sk+(1./tau)*pBA0sk+(1.-pvsk)*(1.-fsk)*pBI0sk
+        pBA1sk=(1.-pvsk)*fsk*pBI0sk+(1.-(1./tau))*(1.-pvsk)*pBA0sk
+        pFI1sk=(1.-alpha)*fsk*pS0sk+(1./tau)*pFA0sk+pvsk*(pBI0sk+(1.-1./tau)*pBA0sk)+(1.-fsk)*pFI0sk
+        pFA1sk=fsk*pFI0sk+(1.-1./tau)*pFA0sk
+        pS1sk=(1.-fsk)*pS0sk
+
+        #update
+        pBI0g = pBI1g
+        pBA0g = pBA1g
+        pFI0g = pFI1g
+        pFA0g = pFA1g
+        pS0g = pS1g
+
+        pBI0sk=pBI1sk
+        pBA0sk=pBA1sk
+        pFI0sk=pFI1sk
+        pFA0sk=pFA1sk
+        pS0sk=pS1sk
+
+        if float(pBI0sk)<=0.0000000001:
+            pBI0sk=0.0
+        if float(pBA0sk)<=0.0000000001:
+            pBA0sk=0.0
+        if float(pFI0sk)<=0.0000000001:
+            pFI0sk=0.0
+        if float(pFA0sk)<=0.0000000001:
+            pFA0sk=0.0
+        if float(pS0sk)<=0.0000000001:
+            pS0sk=0.0
+        if float(pBI0g)<=0.0000000001:
+	        pBI0g=0.0
+		if float(pBA0g)<=0.0000000001:
+			pBA0g=0.0
+		if float(pFI0g)<=0.0000000001:
+			pFI0g=0.0
+		if float(pFA0g)<=0.0000000001:
+			pFA0g=0.0
+		if float(pS0g)<=0.0000000001:
+			pS0g=0.0
+
+    return (np.asarray(b))
+
+#SIR model
+def rhs(Y, t, beta, mu):
+
+    S = Y[0]
+    I = Y[1]
+    R = Y[2]
+
+    N = S + I + R
+
+    dS = - beta * I * S / N
+    dI = beta * I * S / N - mu * I
+    dR = mu * I
+
+    dY = [ dS, dI, dR ]
+
+    return dY
+
+def sir(T, bainit, mybeta, mymu, net_size):
+    # Parameters
+    beta = mybeta
+    mu = mymu
+    N=net_size
+
+    ba=[]
+    fa=[]
+
+    S0 = 1. - bainit
+    I0 = bainit
+    R0 = 0.00
+
+    Y0 = [S0, I0, R0]
+
+    solution = scipy.integrate.odeint(rhs,Y0, T, args = (beta, mu))
+
+    S = solution[:, 0]
+    I = solution[:, 1]
+    R = solution[:, 2]
+
+    for k in range(len(I)):
+        I[k] = int(float(I[k]) * net_size)
+        S[k] = int(float(S[k]) * net_size)
+        R[k] = int(float(R[k]) * net_size)
+
+    return (np.asarray(I))
+
