d6b434c3f261a2c006f22668577d3d7a40238ab1
Marcella Tambuscio <marcella.tambuscio@gmail.com>
2016-11-03 18:15:00 +0100

    Update hoaxy_tryfit.py
    Updated with the cumulative versions (not for SIR)

--- hoaxy_datafit/hoaxy_tryfit.py
+++ hoaxy_datafit/hoaxy_tryfit.py
@@ -8,7 +8,7 @@ from scipy.optimize import curve_fit
 from  models_functions import mf_seg, mf_noseg, sir, mf_seg_plot, mf_noseg_plot
 
 
-hoax_data = pd.read_csv('hoaxy_mexico-border-trump-elected_cum.csv',parse_dates=True)
+hoax_data = pd.read_csv('hoaxy_dr-drew-hillary-clinton-health_cum.csv',parse_dates=True)
 for_users = hoax_data['For']
 against_users = hoax_data['Against']
 t_empirical=np.arange(0,len(hoax_data))
@@ -16,8 +16,8 @@ len_data= len(hoax_data)
 
 #DATA FITTING
 #mf_seg
-param0=[0.1,0.5, 7000, 0.5, 0.1, 0.1, 0.8, 10]
-param_bounds =([0.001,0, 1000, 0.1, 0, 0, 0.5, 5], [10.,1., 2000000, 0.9, 0.9, 0.2, 1., 15])
+param0=[0.15,0.1, 5500, 0.5, 0.05, 0.2, 0.5, 10]
+param_bounds =([0.001,0, 1, 0.1, 0, 0, 0.5, 5], [10.,1., 2000000, 0.9, 0.9, 0.2, 1., 15])
 
 popt, pcov = scipy.optimize.curve_fit(mf_seg,
                                           t_empirical,
@@ -49,7 +49,8 @@ t_plot= np.unique(np.sort(np.append(t_plot, t_empirical)))
 ba_plot=mf_seg_plot(t_plot, alpha, net_size, gullible_size, bainit, pvsk, segregation, tau)
 
 pylab.figure()
-pylab.axis([0.0,max(t_empirical), 0, net_size / 2])
+pylab.axis([0.0,max(t_empirical), 0, max(for_users)+200])
+#pylab.axis([0.0,max(t_empirical), 0, max(ba_plot)])
 ax = pylab.gca()
 ax.set_autoscale_on(False)
 pylab.plot(t_empirical, for_users, '.', label='data point')
@@ -58,7 +59,7 @@ pylab.plot(t_plot,ba_plot , label='fit_seg')
 #print(abs(ba - for_users))
 
 #mf_noseg
-param0=[0.1,0.3, 7000, 0.05, 0, 10]
+param0=[0.05,0.05, 2550, 0.05, 0, 10]
 param_bounds =([0.001,0, 1000, 0, 0,  5], [10,1., 2000000, 0.3, 0.05, 15])
 #t_sim = scipy.linspace(0, len(hoax_data), 180)
 popt, pcov = scipy.optimize.curve_fit(mf_noseg,
@@ -89,7 +90,7 @@ ba_plot= mf_noseg_plot(t_plot, alpha, net_size, bainit, pv, tau)
 pylab.plot(t_plot, ba_plot, label='fit_noseg')
 
 #sir
-param0=[0.75, 0.5, 0., 1600]
+param0=[0.2, 0.8, 0., 2300]
 param_bounds =([0, 0, 0, 1000], [1., 1., 1.,  2000000])
 
 popt, pcov = scipy.optimize.curve_fit(sir, t_empirical,
@@ -110,8 +111,12 @@ pylab.plot(t_empirical, infected, label='sir')
 
 pylab.title('FIT PARAMETERS')
 pylab.legend()
-pylab.legend(['For Users', 'Model with segregation', 'Model without segregation', "SIR"])
-pylab.savefig('hoaxy_mexico_tryfit.pdf', format='pdf')
+pylab.legend(['For Users', 'Model with segregation', 'Model without segregation',"SIR"],
+             loc='lower right'
+             )
+
+#pylab.legend(['For Users', 'Model with segregation', 'Model without segregation', "SIR"])
+pylab.savefig('hoaxy_drdrew_tryfit_cum.pdf', format='pdf')
 
 
 
