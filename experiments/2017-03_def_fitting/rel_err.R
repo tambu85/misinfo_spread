78679a8fc4766676206058bcefff19cbeaa1dae7
Marcella Tambuscio <marcella.tambuscio@gmail.com>
2017-03-30 23:26:23 +0200

    Update rel_err.R

--- experiments/2017-03_def_fitting/rel_err.R
+++ experiments/2017-03_def_fitting/rel_err.R
@@ -5,16 +5,19 @@ library(plotly)
 setwd("~/Desktop/fit/fitting_data")
 files=list.files("~/Desktop/fit/fitting_data")
 
-# Function that returns Root Mean Squared Error
+# Function that returns Relative Error
 rel_err <- function(data, model)
 { 
-  abs_er=abs(model-data)
-  for(t in 1:length(abs_er)){
-    if(data[t]!=0){
-      abs_er[t]/data[t]
-    }
+  idx = which(data == 0)
+  if(length(idx)>0){
+    #print(idx)
+    data=data[-idx]
+    model=model[-idx]
   }
-  re <- mean(abs_er)
+  abs_er=abs(model-data)
+  rel_er=abs_er/data
+  re <- mean(rel_er)
+  return(re)
 }
 
 
@@ -31,6 +34,7 @@ for(j in seq(1,length(files),2)){
   name=strsplit(files[j],"*_with_segregation.csv")
   #name1=strsplit(name[[1]],"hoaxy_*")
   hoax[i]= name[[1]][1]
+  print(i)
   print(hoax[i])
   
   #seg
@@ -41,10 +45,9 @@ for(j in seq(1,length(files),2)){
   seg_fa = seg$Against_FA
   
   seg_b_nre[i] = rel_err(seg_for, seg_ba)
-  print(paste0("nre FOR:",seg_b_nre))
- 
+  print(paste0("nre FOR:",seg_b_nre[i]))
   seg_f_nre[i] = rel_err(seg_against, seg_fa)
-  print(paste0("nre AGAINST:",seg_f_nre))
+  print(paste0("nre AGAINST:",seg_f_nre[i]))
   
   #noseg
   noseg <- read.table(files[j+1], header = TRUE, sep=",")
@@ -54,10 +57,10 @@ for(j in seq(1,length(files),2)){
   noseg_fa = noseg$Against_FA
   
   noseg_b_nre[i] = rel_err(noseg_for, noseg_ba)
-  print(paste0("nre FOR:",noseg_b_nre))
+  print(paste0("nre FOR:",noseg_b_nre[i]))
 
   noseg_f_nre[i] = rel_err(noseg_against, noseg_fa)
-  print(paste0("nre AGAINST:",noseg_f_nre))
+  print(paste0("nre AGAINST:",noseg_f_nre[i]))
   
   i <-i+1
 }
@@ -74,13 +77,15 @@ print(paste0("noseg_AF:",ave_noseg_f_nre))
 print(paste0("seg_AB:",ave_seg_b_nre))
 print(paste0("seg_AF:",ave_seg_f_nre))
 
-
+#old plot
 ave_df <- data.frame(ave_noseg_b_nre, ave_noseg_f_nre, ave_seg_b_nre, ave_seg_f_nre)
 colnames(ave_df) <-c("noseg_b","noseg_f","seg_b","seg_f")
 m_ave <- melt(ave_df)
+
 ap <- ggplot(data=m_ave, aes(x=variable, y=value, fill=variable))+
   geom_bar(stat="identity",position=position_dodge())+
-  scale_fill_brewer(palette="Spectral")+
+  scale_fill_manual(values=c("blue", "red", "cadetblue1","gold"))+
+  #scale_fill_brewer(palette="Spectral")+
   #ylim(c(0,0.003))+
   xlab("model compartment")+
   #scale_x_discrete(labels=c("","","",""))+
@@ -89,6 +94,34 @@ ap <- ggplot(data=m_ave, aes(x=variable, y=value, fill=variable))+
 ap <- ggplotly(ap)
 show(ap)
 
+#stacked plot
+colnames(m_ave) <- c("model", "value")
+m_ave$model<- as.character(m_ave$model)
+m_ave$state <- m_ave$model
+
+m_ave$model[m_ave$model == "noseg_b"] <- "noseg"
+m_ave$model[m_ave$model == "noseg_f"] <- "noseg"
+m_ave$model[m_ave$model == "seg_f"] <- "seg"
+m_ave$model[m_ave$model == "seg_b"] <- "seg"
+m_ave$state[m_ave$state == "noseg_b"] <- "believers"
+m_ave$state[m_ave$state == "noseg_f"] <- "fact-checkers"
+m_ave$state[m_ave$state == "seg_b"] <- "believers"
+m_ave$state[m_ave$state == "seg_f"] <- "fact-checkers"
+
+ap2 <- ggplot(data=m_ave, aes(x=model, y=value, fill=state))+
+  geom_bar(stat="identity")+
+  scale_fill_manual(values=c("blue", "red", "cadetblue1","gold"))+
+  #scale_fill_brewer(palette="Spectral")+
+  #ylim(c(0,0.003))+
+  xlab("model compartment")+
+  #scale_x_discrete(labels=c("","","",""))+
+  theme(axis.text.x =element_text(size=12))+
+  theme_minimal()+
+  ggtitle("Relative Error")
+ap2 <- ggplotly(ap2)
+show(ap2)
+
+#old totale plot
 plot_df <- data.frame(hoax,
                        noseg_b_nre, noseg_f_nre,
                        seg_b_nre, seg_f_nre)                      
@@ -97,13 +130,51 @@ m <- melt(plot_df)
 
 p <- ggplot(data=m, aes(x=hoax, y=value, fill=variable))+
   geom_bar(stat="identity", position=position_dodge())+
+  scale_fill_manual(values=c("blue", "red", "cadetblue1","gold"))+
   theme(axis.text.x =element_text(size=8, angle=90))+
-  scale_fill_brewer(palette="Spectral")+
+  #scale_fill_brewer(palette="Spectral")+
   scale_x_discrete(labels=c(1:31))+
+  #theme_minimal()+
   ggtitle("Relative Error")
 
 p <- ggplotly(p)
 show(p)
+
+#separate plots for entire dataset
+#noseg
+plot_df_noseg <- data.frame(hoax,
+                      noseg_b_nre, noseg_f_nre)                      
+colnames(plot_df_noseg) <-c("hoax","noseg_b","noseg_f")
+mns <- melt(plot_df_noseg)
+
+pns <- ggplot(data=mns, aes(x=hoax, y=value, fill=variable))+
+  geom_bar(stat="identity", position=position_dodge())+
+  scale_fill_manual(values=c("blue", "red"))+
+  #theme(axis.text.x =element_text(size=8, angle=90))+
+  scale_x_discrete(labels=c(1:31))+ theme(axis.text.x =element_text(size=8))+
+  #theme_minimal()+
+  ggtitle("NOSEG - Relative Error")
+
+pns <- ggplotly(pns)
+show(pns)
+
+#seg
+plot_df_seg <- data.frame(hoax,
+                            seg_b_nre, seg_f_nre)                      
+colnames(plot_df_seg) <-c("hoax","seg_b","seg_f")
+ms <- melt(plot_df_seg)
+
+ps <- ggplot(data=ms, aes(x=hoax, y=value, fill=variable))+
+  geom_bar(stat="identity", position=position_dodge())+
+  scale_fill_manual(values=c("blue", "red"))+
+  #theme(axis.text.x =element_text(size=8, angle=90))+
+  scale_x_discrete(labels=c(1:31))+ theme(axis.text.x =element_text(size=8))+
+  #theme_minimal()+
+  labs(fill="")+
+  ggtitle("SEG - Relative Error")
+
+ps <- ggplotly(ps)
+show(ps)
 
 
 
