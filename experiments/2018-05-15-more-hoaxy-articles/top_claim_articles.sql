41c6b701eb49c8cb2f0908cfd62706f843cbb0ff
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2018-05-15 17:20:10 -0400

    Count each tweet exactly once -- regardless of number of URLs it has

--- experiments/2018-05-15-more-hoaxy-articles/top_claim_articles.sql
+++ experiments/2018-05-15-more-hoaxy-articles/top_claim_articles.sql
@@ -1,7 +1,9 @@
+/* Return top 100 most shared claim articles from 2016/05/16 to 2017/03/31 (inclusive). Return URL and number of tweets */
+
 CREATE TEMP VIEW v AS
     SELECT 
         MAX(article.canonical_url) as url, 
-        COUNT(*) AS num_tweets
+        COUNT(DISTINCT tweet.raw_id) AS num_tweets
     FROM site 
         JOIN url
         ON site.id = url.site_id
