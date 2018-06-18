ded1b79dbd9afe5698a12ca0625702fa30840b98
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2018-06-18 17:06:14 -0400

    Fixed PyTables warning when storing hierarchical columns, keep original story index, reorganized python module.

--- experiments/2018-05-15-more-hoaxy-articles/curves.py
+++ experiments/2018-05-15-more-hoaxy-articles/curves.py
@@ -3,6 +3,10 @@
 """ Create curves for data fitting. Requires Pandas 0.18+ for new resampling
 interface. """
 
+# TODO:
+# 2. Report info on discarded stories.
+
+
 from __future__ import print_function
 
 import os
@@ -14,6 +18,9 @@ import numpy
 import json
 from itertools import count
 from collections import defaultdict
+
+__all__ = ["parser", "filterstories", "createstore", "findplateau",
+           "iterstories"]
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('input', help='CSV input file')
@@ -59,11 +66,8 @@ def filterstories(df, min_tweets_total=1000, min_tweets_each=100):
     idx = tmin_df['fact'] > tmin_df['fake']
     df = df.set_index('story_id').loc[idx].reset_index()
 
-    # sort and reindex to remove gaps
+    # make sure return value has sorted index
     df = df.set_index('story_id').sort_index().reset_index()
-    story_id_values = df['story_id'].unique()
-    mapper = dict(zip(story_id_values, range(len(story_id_values))))
-    df['story_id'] = df['story_id'].map(mapper.__getitem__)
     return df
 
 
@@ -83,7 +87,7 @@ def _activeusers(df, freq='H'):
     return df.cumsum()
 
 
-def _findplateau(y, x=None, mindeg=1.0):
+def findplateau(y, x=None, mindeg=1.0):
     """
     Find the value at which a curve reaches its plateau. A plateau is
     reached when the angle tangent to the curve at any given point is
@@ -102,7 +106,7 @@ def _findplateau(y, x=None, mindeg=1.0):
 
     Returns
     =======
-    xplateau : int
+    ixplateau : int
         Index of the beginning of the plateau
     """
     y = numpy.ravel(y)
@@ -133,7 +137,7 @@ def _trimplateau(ts, mindeg):
     """
     y = ts.values.copy()
     x = ts.index.get_level_values(-1).ravel().astype('M8[h]').astype('int')
-    return _findplateau(y, x=x, mindeg=mindeg)
+    return findplateau(y, x=x, mindeg=mindeg)
 
 
 def iterstories(df, freq='H', trim=True, mindeg=1.0):
@@ -144,7 +148,6 @@ def iterstories(df, freq='H', trim=True, mindeg=1.0):
     """
     df = df.groupby('story_id').apply(_activeusers, freq)
     grouper = df.groupby(level=0)
-    i = 1
     for story_id, subdf in grouper:
         subdf = subdf.unstack(level=[1, 2])['active_users']
         subdf = subdf.loc[story_id].fillna(method='ffill').fillna(0)
@@ -161,22 +164,28 @@ def iterstories(df, freq='H', trim=True, mindeg=1.0):
             subdf.columns = idx   # resets column index
             subdf.rename(columns=lambda k: k.replace('%', ''), inplace=True)
             # finally, yield the data frame and the new story_id
-            yield i, subdf
-            i += 1
+            yield story_id, subdf
 
 
-def main(args):
-    df = pandas.read_csv(args.input, parse_dates=['created_at'])
-    df = filterstories(df, args.min_tweets_total, args.min_tweets_each)
-    # Store data frame in HDF5 file, each data frame has its own key
-    #   /story_XX
-    # where XX is the ID of the story (01, 02, etc.)
+def createstore(input, output, freq, trim, mindeg, min_tweets_total,
+                min_tweets_each, **kwargs):
+    """
+    This is the main function of the script. It takes all the arguments defined
+    by the ArgumentParse and creates a new HDFStore holding the processed data.
+
+    Data will be stored in an HDF5 file, each data frame has its own key
+      /story_XX
+    where XX is the ID of the story (01, 02, etc.).
+
+    Note: additional keyword arguments are ignored.
+    """
+    df = pandas.read_csv(input, parse_dates=['created_at'])
+    df = filterstories(df, min_tweets_total, min_tweets_each)
     n_stories = len(df['story_id'].unique())
     key_digits = int(math.ceil(math.log10(n_stories)))
     key_template = 'story_{{:0{:d}d}}'.format(key_digits)
-    iterator = iterstories(df, freq=args.freq, trim=args.trim,
-                           mindeg=args.mindeg)
-    store = pandas.HDFStore(args.output, 'w')
+    iterator = iterstories(df, freq=freq, trim=trim, mindeg=mindeg)
+    store = pandas.HDFStore(output, 'w')
     urls = {}
     with store:
         for story_id, story_df in iterator:
@@ -191,23 +200,28 @@ def main(args):
                              for k, v in story_df.columns]
             for i in range(len(short_columns)):
                 k, v = short_columns[i]
-                c = counts[v].next()
+                c = next(counts[v])
                 if c:
                     v = v + '_{}'.format(str(c))
                     short_columns[i] = (k, v)
-            story_df.columns = short_columns
+            story_df.columns = pandas.MultiIndex.from_tuples(short_columns)
             key = key_template.format(story_id)
             store.put(key, story_df)
-    print("Data written to {}.".format(args.output))
+    print("Data written to {}.".format(output))
     # serialize full URLs to JSON
-    output_fname, _ = os.path.splitext(args.output)
+    output_fname, _ = os.path.splitext(output)
     json_output = output_fname + os.path.extsep + 'json'
     with open(json_output, 'w') as f:
         json.dump(urls, f)
     print("Data written to {}.".format(json_output))
 
 
-if __name__ == '__main__':
+def _main():
+    """
+    This is the entry point of the script when it is called from the command
+    line.It will read arguments from command line and run the actual "main"
+    function, and will return whatever it returns.
+    """
     args = parser.parse_args()
     if os.path.exists(args.output) and not args.force_overwrite:
         print("Error: File exists: {}".format(args.output),
@@ -216,4 +230,8 @@ if __name__ == '__main__':
     elif args.force_overwrite:
         print("Warning: Overwriting: {}".format(args.output),
               file=sys.stderr)
-    main(args)
+    return createstore(**vars(args))
+
+
+if __name__ == '__main__':
+    _main()
