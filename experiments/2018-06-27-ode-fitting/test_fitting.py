bc8fddee99cd0cae88bd886f015d41d9371a6b25
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2019-12-21 17:00:33 -0500

    More readable output, added comments and docstring

--- experiments/2018-06-27-ode-fitting/test_fitting.py
+++ experiments/2018-06-27-ode-fitting/test_fitting.py
@@ -7,54 +7,113 @@ from models import hoaxmodel
 
 def gendata(model, y0, t, sigma=0, *args):
     """
-    model - ODE
-    y0 - initial conditions
-    t - time points
-    sigma - variance of gaussian noise term
-    obsvars - return only these variables (list of column indices)
-    args - ODE model parameters
+    Generate synthetic data with Gaussian noise from a given ODE model. The
+    function uses `scipy.integrate.odeint` to integrate the ODE system from a
+    given set of initial conditions, and then applies additional Gaussian noise
+    with 0 mean and tunable sigma.
+
+    Parameters
+    ==========
+    model : callable
+        a function implementing an ODE system.
+
+    y0 : ndarray
+        the vector of initial conditions of the model.
+
+    t : ndarray
+        the vector time points at which to integrate the model.
+
+    sigma : float
+        variance of gaussian noise term (default is no noise).
+
+    Notes
+    =====
+    Additional positional arguments (`args`) will be passed to `model` via
+    odeint. This can be used to specify the actual parameters of the model.
     """
     y = scipy.integrate.odeint(model, y0, t, args=args)
     yerr = scipy.stats.norm.rvs(scale=sigma, size=y.shape)
     return numpy.c_[t, y + yerr]
 
 
+def printparams(xopt, xerr=None, names=None):
+    if names is None:
+        names = [str(i) for i in range(len(xopt))]
+    tw = max(map(len, names))
+    names = [n.rjust(tw) for n in names]
+    if xerr is not None:
+        for n, xo, xe in zip(names, xopt, xerr):
+            print("{}: {:.2e} +/- {:.2e}".format(n, xo, xe))
+    else:
+        for n, xo in zip(names, xopt):
+            print("{}: {:.2e}".format(n, xo))
+    print()
+
+
 if __name__ == '__main__':
+    # Model parameters
     pv = 0.1
     tauinv = 0.1
     alpha = 0.5
-    t = numpy.linspace(0, 10, 11)
-    sigma = 2
+    x = (pv, tauinv, alpha)
+
+    # True value of the initial conditions of the system. In order they are:
+    # BA0, FA0, BI0, FI0, S0
     y0 = (1000, 0, 0, 0, 9000)
-    data = gendata(hoaxmodel, y0, t, sigma, pv, tauinv, alpha)
+
+    names = [
+        "BA0",
+        "FA0",
+        "BI0",
+        "FI0",
+        "S0",
+        "pv",
+        "tauinv",
+        "alpha"
+    ]
+
+    # The true parameter values
+    printparams(y0 + x, names=names)
+
+    # Vector of integration time steps
+    t = numpy.linspace(0, 10, 11)
+
+    # Generate data by integrating ODE system and applying Gaussian noise with
+    # given sigma.
+    sigma = 2
+    data = gendata(hoaxmodel, y0, t, sigma, *x)
+
+    # Keep only time, BA, FA
     data = data[:, :3]
 
+    # Upper bound used by the optimiation routine for state variables unknowns
     N_max = 1e6
 
-    # All initial conditions are unknown parameters to fit
+    # Scenario 1) The full vector of initial conditions is part of the unknown
+    # parameters to fit. To achieve this, set `fity0=True`.
+
     bounds = [
-        (0, N_max),  # BA
-        (0, N_max),  # FA
-        (0, N_max),  # BI
-        (0, N_max),  # FI
-        (0, N_max),  # S
+        (0, N_max),  # BA0
+        (0, N_max),  # FA0
+        (0, N_max),  # BI0
+        (0, N_max),  # FI0
+        (0, N_max),  # S0
         (0, 1),  # pv
         (0, 1),  # tauinv
         (0, 1),  # alpha
     ]
-    xopt1, err1 = fitone(data, hoaxmodel, 5, bounds, fity0=True, nrep=1)
+    xopt1, err1 = fitone(data, hoaxmodel, 5, bounds, fity0=True, nrep=25)
+    printparams(xopt1, err1, names)
 
-    # Only BI/FI/S are unknown parameters to fit, but no BA/FA
+    # Scenario 2) Only BI0/FI0/S0 are unknown parameters to fit; BA0/FA0 are
+    # set to be equal to the first observation of the data.
     bounds = [
-        (0, N_max),  # BI
-        (0, N_max),  # FI
-        (0, N_max),  # S
+        (0, N_max),  # BI0
+        (0, N_max),  # FI0
+        (0, N_max),  # S0
         (0, 1),  # pv
         (0, 1),  # tauinv
         (0, 1),  # alpha
     ]
-    xopt2, err2 = fitone(data, hoaxmodel, 5, bounds, nrep=1)
-
-    numpy.set_printoptions(precision=2, suppress=True)
-    print("{} +/- {}".format(numpy.round(xopt1, 2), numpy.round(err1, 2)))
-    print("{} +/- {}".format(numpy.round(xopt2, 2), numpy.round(err2, 2)))
+    xopt2, err2 = fitone(data, hoaxmodel, 5, bounds, nrep=25)
+    printparams(xopt2, err2, names[2:])
