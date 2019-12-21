31f114783aa936d74411c0d6f9e6a7f3b986f610
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2019-12-21 12:15:08 -0500

    Added plotting script

---
+++ experiments/2018-05-15-more-hoaxy-articles/plotall.py
@@ -0,0 +1,38 @@
+""" Plot fact/fake curve for each story in one single figure """
+
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+
+
+def plotone(group, key):
+    ax = plt.gca()
+    group.reset_index(inplace=True)
+    group.plot(y='fake', ax=ax, legend=False)
+    group.plot(y='fact', ax=ax, legend=False)
+
+
+def plot(path, nrows=5, ncols=5):
+    df = pandas.read_csv('data_tweets.csv')
+    grouped = df.groupby('story_id')
+    fig, axs = plt.subplots(nrows, ncols, sharex=True)
+
+    for i, (key, group) in enumerate(grouped):
+        h, k = numpy.unravel_index(i, axs.shape)
+        plt.sca(axs[h, k])
+        plotone(group, key)
+    plt.show()
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("path", help='path to data file')
+    parser.add_argument('-r', '--rows', dest='nrows', type=int,
+                        help='plot rows (default: %(default)s)',
+                        default=5)
+    parser.add_argument('-c', '--cols', dest='ncols', type=int,
+                        help='plot cols (default: %(default)s)',
+                        default=5)
+    args = parser.parse_args()
+    plot(**vars(args))
