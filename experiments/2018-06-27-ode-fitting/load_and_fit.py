6d1b3f4607addf399c2c0a44cbcd25d187262fdf
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2019-12-29 10:51:23 -0500

    Refactored script using functions, added command line parsing to read arguments

--- experiments/2018-06-27-ode-fitting/load_and_fit.py
+++ experiments/2018-06-27-ode-fitting/load_and_fit.py
@@ -1,126 +1,90 @@
 from __future__ import print_function
 
-import json
-import pandas
+import datetime
 import numpy
-from contextlib import closing
-from plotting import plotall, plotonewithurls
-from fitting import fitone, fitmany, gendata
-import models
+import pandas
+import matplotlib
 import matplotlib.pyplot as plt
+import argparse
+from contextlib import closing
 
-# place the data files in the `data' folder in the root of the repository
-# folder
-hdf_store_path = '../../data/data.hdf'
-urls_json_path = '../../data/data.json'
-
-store = pandas.HDFStore(hdf_store_path)
-
-# this reads the data into a dictionary of data frames. The columns of these
-# data frames are organized in two groups of columns, the `fake' and `fact'
-# groups. For each group, the data are disaggregated by individual URLs. So for
-# example if a story was published in 2 fake news articles and 3 fact-checking
-# articles the data frame will have two groups of columns, the first with 2
-# columns and the second with 3, for a total of 5 columns.
-with closing(store):
-    data = dict((int(k.split('_')[1]), store[k]) for k in store)
-
-print(len(data))
-#print(data[3])
-
-# This will aggregate all the `fake' and `fact' column groups, and will
-# concatenate all the data frames in one single data frame. These are the data
-# that can be used for the fit.
-df = pandas.concat(dict((k, df.sum(axis=1, level=0))
-                        for k, df in data.items()), names=['story_id'])
-
-# This data frame is indexed by the ID of each story. So to get story with ID 7
-# (see spreadsheet) you just use the dataframe
-
-print(df.loc[7].index[0])
+import models
 
 
-# ADDITIONAL:
-# these are the URLs of each story, useful for figure titles or legends
-with closing(open(urls_json_path)) as f:
-    urls = json.load(f)
+def readdata(path, storyid):
+    store = pandas.HDFStore(path)
+    with closing(store):
+        try:
+            df = store.get("/story_{:02d}".format(storyid))
+        except KeyError:
+            import sys
+            print("No such story: {}".format(storyid), file=sys.stderr)
+            sys.exit(-1)
+        df = df.sum(axis=1, level=0)
+        return df
 
-k=sorted(data.keys())
-#print(k)
-k0=k[0]
-#print(urls[str(k0)]['fact'])
 
+def plotfit(ax, data_df, fit_df, title):
+    data_df.plot(color='k', ls='', marker='o', ax=ax)
+    fit_df.plot(color='r', ls='-', ax=ax)
+    ax.legend(["Data", "Fit"], loc='best')
+    ax.set_title(title)
+    ax.set_yscale('symlog')
+    ymin, ymax = ax.get_ylim()
+    ax.set_ylim(ymin, ymax * 10)
+    ax.set_xlabel('Hours since $t_0$')
+    ax.set_ylabel('Active Users')
+    return ax.lines
+
+
+def main(path, storyid, output=None):
+    story_df = readdata(path, storyid)
+    t0 = story_df.index[0]
+
+    m = models.HoaxModel()
+    m.FA = story_df.loc[t0]['fact']
+    m.BA = story_df.loc[t0]['fake']
+    data = numpy.c_[story_df['fake'], story_df['fact']]
+    m.fit(data)
+    xopt = numpy.hstack([m.y0, m.theta])
+    names = m._y0 + m._theta
+    err = m.err_
+    for n, x, e in zip(names, xopt, err):
+        print("{}: {:.2e} +/- {:.2e}".format(n, x, e))
+
+    t = numpy.arange(len(story_df))
+    fit_data = m.simulate(t)
+    fit_df = pandas.DataFrame(fit_data, columns=["fake", "fact"])
+    story_df.reset_index(inplace=True)
+
+    toc = datetime.datetime.now()
+    print("Ended: {}".format(toc))
+    print("Elapsed: {}".format(toc - tic))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    title = r"story {}: $t_0$ = {}".format(storyid, t0)
+    fig.suptitle(title)
+
+    plotfit(ax1, story_df['fake'], fit_df['fake'], "fake")
+    plotfit(ax2, story_df['fact'], fit_df['fact'], "fact-check")
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.88)
+    plt.draw()
+    if output is not None:
+        plt.savefig(output)
+    if matplotlib.is_interactive():
+        plt.show()
 
 
 if __name__ == '__main__':
+    tic = datetime.datetime.now()
+    print("Started: {}".format(tic))
 
-    import models
-    N_max = 1e6
-
-    bounds = [
- #           (0, N_max),  # BA
- #           (0, N_max),  # FA
-            (0, N_max),  # BI
-            (0, N_max),  # FI
-            (0, N_max),  # S
-            (0, 1),  # pv
-            (0, 1),  # tauinv
-            (0, 1),  # alpha
-        ]
-
-
-
-    chosenid=7
-    mystory = df.loc[chosenid]
-    t0 = mystory.index[0]
-    t = numpy.linspace(0, len(mystory) - 1, len(mystory))
-    data7 = numpy.c_[t, mystory.values]
-    #print(data7)
-
-    xopt, err = fitone(data7, models.hoaxmodel, 5, bounds, fity0=False, nrep=1)
-    numpy.set_printoptions(precision=2, suppress=True)
-    print("{} +/- {}".format(numpy.round(xopt, 2), numpy.round(err, 2)))
-
-    fa0,ba0= data7[0][1:]
-    [bi_opt, fi_opt, s_opt, pv_opt, tauinv_opt, alpha_opt]=numpy.round(xopt,2)
-    #[ba_opt, fa_opt, bi_opt, fi_opt, s_opt, pv_opt, tauinv_opt, alpha_opt]=numpy.round(xopt,2)
-    y0 = (fa0, ba0, bi_opt, fi_opt, s_opt)
-    #y0 = (ba_opt, fa_opt, bi_opt, fi_opt, s_opt)
-    sigma=0
-    fit_data = gendata(models.hoaxmodel, y0, t, sigma, pv_opt, tauinv_opt, alpha_opt)
-    fit_data = fit_data[:,1:3]
-    fit_df = pandas.DataFrame.from_records(fit_data)
-    fit_df.columns=["fa_fit","ba_fit"]
-    fit_df.index=mystory.index
-
-    print(fit_df.head())
-
-    k=7
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    title = "FITTING Story " + str(k) + ": $t_0=$ {}".format(t0)
-    fig.suptitle(title)
-
-
-    ax1 = plt.subplot(1, 2, 1)
-    mystory['fake'].plot(legend=True, color='k', ls='--', ax=ax1, title="Fake-data")
-    fit_df['ba_fit'].plot(legend=True, color='k', ls='-',ax=ax1, title="Fake-fit")
-    ax1.set_yscale('symlog')
-    ymin, ymax = ax1.get_ylim()
-    ax1.set_ylim(ymin, ymax * 10)
-    ax1.set_xlabel('Hours since $t_0$')
-    ax1.set_ylabel('Active Users')
-
-
-    ax2 = plt.subplot(1, 2, 2)
-    mystory['fact'].plot(legend=True, color='r', ls='--', ax=ax2, title="Factcheck-data")
-    fit_df['fa_fit'].plot(legend=True, color='r',ls='-', ax=ax2, title="Factcheck-fit")
-    ax2.set_yscale('symlog')
-    ymin, ymax = ax2.get_ylim()
-    ax2.set_ylim(ymin, ymax * 10)
-    ax2.set_xlabel('Hours since $t_0$')
-
-
-    plt.subplots_adjust(top=0.88)
-    plt.tight_layout()
-
-    plt.show()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('path')
+    parser.add_argument('storyid', type=int, help='ID of story to plot')
+    parser.add_argument('-o', '--output', metavar='FILE',
+                        help='save figure to %(metavar)s')
+    args = parser.parse_args()
+    main(args.path, args.storyid, args.output)
