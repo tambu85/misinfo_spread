25c99232ce6da45bcff3fa181b5f36523b78bb9e
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2020-01-02 16:45:38 -0500

    Override path from pickled results, code cleanup

--- experiments/2018-06-27-ode-fitting/report_fit_results.py
+++ experiments/2018-06-27-ode-fitting/report_fit_results.py
@@ -1,37 +1,46 @@
+""" Report fit results from pickled models """
+
+import os
 import numpy
 import pandas
 import pickle
 import glob
+import argparse
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('path', help='path to pickle files')
+parser.add_argument('--data', help='override data path', dest='data_path')
+args = parser.parse_args()
+os.chdir(args.path)
 
 runs = [pickle.load(open(p, 'rb')) for p in glob.glob('*.pickle')]
-runs = runs[2:]
-path = runs[0]['path']
+path = runs[0]['path'] if args.data_path is None else args.data_path
 df = pandas.read_csv(path, index_col=0, parse_dates=True)
+
 
 def err(k, model, metric='mape'):
     _df = df.loc[k]
     data = numpy.c_[_df['fake'], _df['fact']]
     return model.error(data, metric=metric)
 
+
 labels = []
 values = []
+
 
 for run in runs:
     fit_type = run['fity0']
     models = run['models']
     for story, model in models.items():
-        for metric in ['mape', 'smape', 'logaccratio']:
+        for metric in ['mape', 'smape', 'logaccratio', 'rmse']:
             label = (metric, fit_type, story)
             e = err(story, model, metric)
             labels.append(label)
             values.append(e)
-        metric = 'rmse'
-        label = (metric, fit_type, story)
-        e = numpy.sqrt(model.cost_)
-        labels.append(label)
-        values.append(e)
 
-idx = pandas.MultiIndex.from_tuples(labels, names=['fit_type', 'metric', 'story'])
+
+idx = pandas.MultiIndex.from_tuples(labels,
+                                    names=['fit_type', 'metric', 'story'])
 err_df = pandas.DataFrame(values, index=idx, columns=['error'])
 err_df = err_df.unstack(level=[0, 1])['error']
 print('Avg. Error (%):')
