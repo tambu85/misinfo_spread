1cff2d8aeb2ed7286e2acc697441de84bfc6ca2e
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2020-02-01 11:03:57 -0500

    Multiple fixes: - Fix issues with `_resample` (use nunique on user_id); - Fix issue with `_align` (truncate at fraction of tweets, remove trailing zeroes; - Declare defaults module-wide; - Code refactoring.

--- replication/01_data_cleaning/curves.py
+++ replication/01_data_cleaning/curves.py
@@ -13,26 +13,37 @@ import datetime
 
 __all__ = ["parser", "filterstories", "createdata"]
 
+MAX_LAG = 168
+MIN_TWEETS_EACH = 100
+MIN_TWEETS_TOTAL = 1000
+FREQ = 'h'
+CUTOFF_FRAC = .99
+
+
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('input', help='CSV input file')
 parser.add_argument('output', help='HDF store output')
-parser.add_argument('-f', '--freq', default='H',
+parser.add_argument('-f', '--freq', default=FREQ,
                     help='Sampling frequency (default: %(default)s)')
 parser.add_argument('-F', '--force-overwrite', action='store_true',
                     help='Force overwrite of output file')
-parser.add_argument('--min-tweets-each', type=int, default=100,
+parser.add_argument('--min-tweets-each', type=int, default=MIN_TWEETS_EACH,
                     help='Filter story with at least %(metavar)s '
                     'tweets each type (default: %(default)d)', metavar='NUM')
-parser.add_argument('--min-tweets-total', type=int, default=1000,
+parser.add_argument('--min-tweets-total', type=int, default=MIN_TWEETS_TOTAL,
                     help='Filter story with at least %(metavar)s '
                     'tweets (default: %(default)d)', metavar='NUM')
-parser.add_argument('--max-lag', type=int, default=168, help='Keep only'
+parser.add_argument('--max-lag', type=int, default=MAX_LAG, help='Keep only'
                     ' stories where fact curve lags fake curve by no more'
                     ' than %(metavar)s. (default: %(default).1f h)',
                     metavar='LAG')
+parser.add_argument('--cutoff-frac', type=float, default=CUTOFF_FRAC,
+                    metavar='FRAC', help='truncate time series after fraction '
+                    '%(metavar)s of tweets (default: %(default)g)')
 
 
-def filterstories(df, min_tweets_total=1000, min_tweets_each=100, max_lag=168):
+def filterstories(df, min_tweets_total=MIN_TWEETS_TOTAL,
+                  min_tweets_each=MIN_TWEETS_EACH, max_lag=MAX_LAG):
     """
     Filter stories from original data to have only
 
@@ -77,8 +88,8 @@ def filterstories(df, min_tweets_total=1000, min_tweets_each=100, max_lag=168):
     df_ts['lag'] = df_ts['fact'] - df_ts['fake']
 
     # We can now select only the stories where the lag between fact and fake is
-    # positive and less than the max lag (default: 168h). We put this info into
-    # a boolean frame that can be used as an index.
+    # positive and less than the max lag (default: MAX_LAG). We put this info
+    # into a boolean frame that can be used as an index.
     idx = (df_ts['fact'] >= df_ts['fake']) & (df_ts['lag'] <= max_lag)
 
     # Using the index, we select the URLs of the filtered stories, and finally
@@ -106,70 +117,98 @@ def filterstories(df, min_tweets_total=1000, min_tweets_each=100, max_lag=168):
     return df
 
 
-def _resample(df, lag, freq):
+def resample(df, lag=MAX_LAG, freq=FREQ):
+    """
+    See _resample.
+    """
+    _df = df.groupby(['story_id', 'tweet_type']).apply(_resample, lag, freq)
+    _df.index.rename('timestamp', level=-1, inplace=True)
+    return _df
+
+
+def _resample(df, lag=MAX_LAG, freq=FREQ):
     """
     Compute curves to fit. Data are number of unique users tweet at hourly
     frequency. (This is taken to represent the number of "active"
     believers/fact-checkers at time t.)
     """
     df.set_index('created_at', inplace=True)
-    # FIXME it should be the number of users, not the number of tweets!
-    df = df.resample('h').agg({'tweet_id': 'count'})
-    df = df.rename(columns={'tweet_id': 'active_users'})
+    df = df.resample(freq).agg({'user_id': 'nunique', 'tweet_id': 'count'})
+    df = df.rename(columns={'user_id': 'active_users',
+                            'tweet_id': 'tweet_count'})
     t0 = df.index[0]
     t1 = df.index[-1]
-    # FIXME: chose t1 so that we cover 95% of the tweets (both fake and fact)
     idx = pandas.date_range(start=t0, end=t1, freq=freq)  # periods=lag,
     df = df.reindex(idx)
     return df.fillna(0)
 
 
-def _align(df, lag):
+def align(df, lag=MAX_LAG, cutoff=CUTOFF_FRAC, freq=FREQ):
+    """
+    See _align.
+    """
+    return df.groupby('story_id').apply(_align, lag, cutoff,
+                                        freq).reset_index()
+
+
+def _align(df, lag=MAX_LAG, cutoff=.99, freq=FREQ):
     """
     For each story, create a single data frame by aligning the 'fact' curve to
-    the 'fake' curve. Truncate tail of trailing zeroes.
+    the 'fake' curve. Cut time series when total tweet count has reached cutoff
+    fraction or lag has elapsed (whichever comes first). Then truncate any
+    trailing zeroes.
     """
     story_id = df.index.get_level_values(0)[0]
     a = df.loc[story_id, 'fake']
     b = df.loc[story_id, 'fact']
-    a, b = a.align(b)
-    # FIXME cleanup commented text after _resample has been fixed, remove
-    # second argument (lag) from function (and fix how it is called).
-    df = pandas.DataFrame({'fake': list(a['active_users']),  # [:lag]
-                           'fact': list(b['active_users'])},  # [:lag]
-                          index=a.index)  # [:lag]
-    df.fillna(0, inplace=True)
-    # truncate tail of trailing zeroes (both fact and fake)
+    a, b = a.align(b, fill_value=0)
+    df = pandas.DataFrame({'fake': list(a['active_users']),
+                           'fact': list(b['active_users'])},
+                          index=a.index)
+    # cutoff based on fraction
+    c = a['tweet_count'] + b['tweet_count']
+    c = c.cumsum() / c.sum()
+    # idxmin returns index of first occurrence of False
+    tscut = (c < cutoff).idxmin()
+    # cutoff based on lag
+    tslag = c.index[0] + pandas.Timedelta(lag - 1, freq)
+    df = df[:min(tslag, tscut)]
+    # remove trailing zeroes
     idx = (df == 0).all(axis=1)[::-1].cumprod()[::-1].astype(bool)
     df = df.mask(idx).dropna()
     return df
 
 
+def geturls(df):
+    urls_df = df.groupby(['story_id', 'tweet_type']).first()['clean_url']
+    urls_df = urls_df.unstack()
+    urls_df.replace('%', '', regex=True, inplace=True)
+    return urls_df
+
+
 def createdata(input, output, freq, min_tweets_total, min_tweets_each,
-               max_lag, **kwargs):
+               max_lag, cutoff_frac, **kwargs):
     """
     This is the main function of the script. It takes all the arguments defined
     by the ArgumentParse and create the data files.
 
-    Note: additional keyword arguments are ignored.
+    Additional keyword arguments are ignored (due to argparse below).
     """
     df = pandas.read_csv(input, parse_dates=['created_at'])
     df = filterstories(df, min_tweets_total, min_tweets_each, max_lag)
 
-    df1 = df.groupby(['story_id', 'tweet_type']).apply(_resample,
-                                                       max_lag, freq)
-    df1.index.rename('timestamp', level=-1, inplace=True)
-    df2 = df1.groupby('story_id').apply(_align, max_lag)
-    df2.reset_index(inplace=True)
-    df2.to_csv(output, index=False)
+    # get urls now or never (following calls remove the urls from df)
+    urls_df = geturls(df)
+
+    # resample to desired freq, align, save to csv
+    df = resample(df, max_lag, freq)
+    df = align(df, max_lag, cutoff_frac, freq)
+    df.to_csv(output, index=False)
     print("Data written to {}.".format(output))
 
-    # serialize full URLs to JSON
+    # serialize URLs to JSON
     output_fname, _ = os.path.splitext(output)
     urls_output = output_fname + '_urls' + os.path.extsep + 'csv'
-    urls_df = df.groupby(['story_id', 'tweet_type']).first()['clean_url']
-    urls_df = urls_df.unstack()
-    urls_df.replace('%', '', regex=True, inplace=True)
     urls_df.to_csv(urls_output)
     print("Data written to {}.".format(urls_output))
 
